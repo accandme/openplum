7eca5c30067bbe63c32a9342813c34709395fbd1
Artyom Stetsenko <artyom.stetsenko@epfl.ch>
2012-10-29 11:17:25 +0000

    Fixed (mostly) the import&partition script. Notes: - Password of the postgres user must be specified in default.pgpass file (last column, currently "postgres"). - DBGEN tables must be placed in datasets/<scale> dir relative to fix_endline.sh and prep.sh. - fix_endline.sh must be run on freshly generated tables first (usage similar to prep.sh). - prep.sh currently configured to use 8 databases on localhost. Creates them if necessary. - config.properties configured in the same way as prep.sh, also using password "postgres". - DBGEN does not avoid duplicate primary keys in partsupp table, which causes prep.sh to fail to import it. I will fix this later. Everything else is imported correctly. We don't need partsupp for Q3 and Q7.

--- tools/prep.sh
+++ tools/prep.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Settings
-pass='thrufbeyto'
-db='dbcourse1'
-nodelist=('icdatasrv1-9' 'icdatasrv1-20' 'icdatasrv2-9' 'icdatasrv2-20' 'icdatasrv3-9' 'icdatasrv3-20' 'icdatasrv4-9' 'icdatasrv4-20')
+pguser='postgres'
+pgpass=default.pgpass
+nodelist=('localhost' 'localhost' 'localhost' 'localhost' 'localhost' 'localhost' 'localhost' 'localhost')
+dblist=('tpch8_1' 'tpch8_2' 'tpch8_3' 'tpch8_4' 'tpch8_5' 'tpch8_6' 'tpch8_7' 'tpch8_8')
 datasets='datasets'
 errorlog=`basename $0`'.log'
 create_schema='create_schema.sql'
-create_dw='create_dw.sql'
+#create_dw='create_dw.sql'
 
 checklog() { # function to check if logfile is empty
 	if [ -s $errorlog ]
@@ -25,7 +26,7 @@ if [ "$#" -gt 2 -o "$1" = '-?' -o "$1" = '--help' ]
 then
 	echo "Usage: $0 [scale] [nodes]"
 	echo "Loads data from $datasets/<scale>/ to <nodes> and partitions it among them, specifying the first node to be the NS node."
-	echo "By default, scale = 0.1 and nodes = \"1 2 3 4 5 6 7 8\", which will upload and partition database of scale 0.1 to all 8 nodes."
+	echo "By default, scale = 0.1 and nodes = \"1 2 3 ...\" (all nodes specified in settings), which will upload and partition database of scale 0.1 to all nodes."
 	exit 1
 fi
 
@@ -43,33 +44,60 @@ then
 fi
 
 # Nodes
-nodes="${nodelist[0]} ${nodelist[1]} ${nodelist[2]} ${nodelist[3]} ${nodelist[4]} ${nodelist[5]} ${nodelist[6]} ${nodelist[7]}"
-numNodes=8
+nodes=(${nodelist[@]})
+dbs=(${dblist[@]})
+numNodes=(${#nodelist[@]})
 if [ "$#" -gt 1 ]
 then
-	nodes=""
+	nodes=()
+	dbs=()
 	numNodes=0
 	for i in $2
 	do
+		nodes[$numNodes]="${nodelist[`expr $i - 1`]}"
+		dbs[$numNodes]="${dblist[`expr $i - 1`]}"
 		numNodes=`expr $numNodes + 1`
-		nodes="$nodes ${nodelist[`expr $i - 1`]}"
 	done
-	nodes=`echo $nodes | sed "s/^[ \t]*//"` # remove leading space
 fi
 
 # Empty log file
 rm -f $errorlog
 touch $errorlog
 
+# Record pgpass
+export PGPASSFILE=$pgpass
+
 # Check connectivity
 echo "Checking connectivity to nodes..."
-for node in $nodes
+for i in $(seq 0 `expr $numNodes - 1`);
 do
 	(
-		command=`mysql --host=$node --password=$pass $db -e 'SHOW TABLES' 2>&1`
-		if [ $? != 0 ]
+		command=`psql -h ${nodes[$i]} -U $pguser --set ON_ERROR_STOP=1 2>&1`
+		if [ $? -ne 0 ]
 		then
-			echo "Error connecting to $node: $command."
+			echo "Error connecting to database ${dbs[$i]} at ${nodes[$i]}: $command."
+		fi
+	) >> $errorlog &
+done
+wait
+checklog
+
+# Create databases if needed
+echo "Checking databases..."
+for i in $(seq 0 `expr $numNodes - 1`);
+do
+	(
+		command=`psql -h ${nodes[$i]} -U $pguser -c '\l' --set ON_ERROR_STOP=1 | grep " ${dbs[$i]} " 2>&1`
+		if [ $? -ne 0 -a $? -ne 1 ]
+		then
+			echo "Error checking for exitence of database ${dbs[$i]} at ${nodes[$i]}: $command."
+		elif [ "$command" == "" ]
+		then
+			command=`psql -h ${nodes[$i]} -U $pguser -c "CREATE DATABASE ${dbs[$i]}" --set ON_ERROR_STOP=1 2>&1`
+			if [ $? -ne 0 ]
+			then
+				echo "Error creating database ${dbs[$i]} at ${nodes[$i]}: $command."
+			fi
 		fi
 	) >> $errorlog &
 done
@@ -78,16 +106,16 @@ checklog
 
 # Drop existing tables
 echo "Dropping existing tables..."
-for node in $nodes
+for i in $(seq 0 `expr $numNodes - 1`);
 do
 	(
-		tables=$(mysql --host=$node --password=$pass $db -e 'SHOW FULL TABLES WHERE Table_Type = "BASE TABLE"' | awk '{ print $1 }' | grep -v '^Tables')
+		tables=$(psql -h ${nodes[$i]} -U $pguser -d ${dbs[$i]} -c '\d' --set ON_ERROR_STOP=1 | grep ' table ' | awk '{ print $3 }')
 		for table in $tables
 		do
-			command=`mysql --host=$node --password=$pass $db -e "DROP TABLE $table" 2>& 1`
-			if [ $? != 0 ]
+			command=`psql -h ${nodes[$i]} -U $pguser -d ${dbs[$i]} -c "DROP TABLE $table" --set ON_ERROR_STOP=1 2>& 1`
+			if [ $? -ne 0 ]
 			then
-				echo "Error dropping table $table on $node: $command."
+				echo "Error dropping table $table in database ${dbs[$i]} at ${nodes[$i]}: $command."
 			fi
 		done
 	) >> $errorlog &
@@ -96,20 +124,28 @@ wait
 checklog
 
 # Create schema
+if [ ! -f $create_schema ]
+then
+	echo "Schema file $create_schema does not exist."
+	echo "Exiting..."
+	exit 1
+fi
 echo "Creating schema..."
-for node in $nodes
+for i in $(seq 0 `expr $numNodes - 1`);
 do
 	(
-		command=`mysql --host=$node --password=$pass $db < $create_schema 2>&1`
-		if [ $? != 0 ]
+		command=`psql -h ${nodes[$i]} -U $pguser -d ${dbs[$i]} -f "$create_schema" --set ON_ERROR_STOP=1 2>&1`
+		if [ $? -ne 0 ]
 		then
-			echo "Error creating schema on $node: $command."
+			echo "Error creating schema in database ${dbs[$i]} at ${nodes[$i]}: $command."
 		fi
 	) >> $errorlog &
 done
+wait
 checklog
 
 # Create dw
+: <<'COMMENT2'
 echo "Creating data warehouse schema..."
 for node in $nodes
 do
@@ -123,45 +159,59 @@ do
 done
 wait
 checklog
+COMMENT2
+
+echo "Loading data..."
+
+# Prepare import commands
+importfile=`basename $0`.tmp
+rm -f $importfile
+tbls=`ls $datasets/$scale | grep -e .tbl$`
+for tbl in $tbls
+do
+	echo "\copy ${tbl%'.tbl'} from '$datasets/$scale/$tbl' DELIMITER '|'" >> $importfile
+done
 
 # Load data
 curNodeNum=0
-echo "Loading data..."
-for node in $nodes
+
+for i in $(seq 0 `expr $numNodes - 1`);
 do
 	curNodeNum=`expr $curNodeNum + 1`
 	(
-		tbls="$datasets/$scale/*.tbl"
-		command=`mysqlimport --host=$node --password=$pass --local --fields-terminated-by='|' $db $tbls 2>&1`
-		if [ $? != 0 ]
-		then
-			echo "Error loading data on $node: $command."
-		fi
+		command=`psql -h ${nodes[$i]} -U $pguser -d "${dbs[$i]}" -f "$importfile" 2>&1`
+		# below doesn't work
+		#if [ $? -ne 0 ]
+		#then
+		#	echo "Error importing data to database ${dbs[$i]} at ${nodes[$i]}: $command."
+		#fi
 	) >> $errorlog &
 done
 wait
 checklog
+rm -f $importfile
 	
 # Partition
 curNodeNum=0
 echo "Partitioning..."
-for node in $nodes
+for i in $(seq 0 `expr $numNodes - 1`);
 do
-	curNodeNum=`expr $curNodeNum + 1`
 	partitionSize=`echo $scale*150000/$numNodes | bc -l` # there are 150,000 tuples in customers table with scale 1
 	(
-		query="DELETE FROM customer WHERE c_custkey NOT BETWEEN `echo "(($curNodeNum-1)*$partitionSize+1)/1" | bc` AND `echo "($curNodeNum*$partitionSize)/1" | bc`;"
-		query="$query DELETE FROM orders WHERE o_custkey NOT BETWEEN `echo "(($curNodeNum-1)*$partitionSize+1)/1" | bc` AND `echo "($curNodeNum*$partitionSize)/1" | bc`;"
+		query="DELETE FROM customer WHERE c_custkey NOT BETWEEN `echo "($i*$partitionSize+1)/1" | bc` AND `echo "(($i+1)*$partitionSize)/1" | bc`;"
+		query="$query DELETE FROM orders WHERE o_custkey NOT BETWEEN `echo "($i*$partitionSize+1)/1" | bc` AND `echo "(($i+1)*$partitionSize)/1" | bc`;"
 		query="$query DELETE FROM lineitem WHERE l_orderkey NOT IN (SELECT o_orderkey FROM orders);"
-		command=`mysql --host=$node --password=$pass $db -e "$query" 2>& 1`
-		if [ $? != 0 ]
+		command=`psql -h ${nodes[$i]} -U $pguser -d "${dbs[$i]}" -c "$query" --set ON_ERROR_STOP=1 2>&1`
+		if [ $? -ne 0 ]
 		then
-			echo "Error partitioning on $node: $command."
+			echo "Error partitioning data in database ${dbs[$i]} at ${nodes[$i]}: $command."
 		fi
 	) >> $errorlog &
 done
 wait
 checklog
+
+echo "All done."
 
 # Remove log file
 rm -f $errorlog
